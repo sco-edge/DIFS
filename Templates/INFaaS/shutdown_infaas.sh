fe5935f159f233e5bfa192c5b7dd9f4176f2e58d
alpharay <gigaofori11@gmail.com>
2025-12-04 14:10:58 +0900

    first commit of our adaptation of the INFaaS code

--- Templates/INFaaS/shutdown_infaas.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Stopping local INFaaS..."
-pkill -f modelreg_server || true
-pkill -f queryfe_server || true
-pkill -f worker_daemon || true
-pkill redis-server || true
-echo "All stopped."
